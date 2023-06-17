7e7e37ccaffab26502d42cf0be72c2ad0ab2bdea
HieuNguyen <hieunm@bytetech.io>
2023-06-18 00:36:27 +0700

    Push code 18/06/2023

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,41 +1,114 @@
 package org.example;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
 
-
+        // Khởi tạo trình duyệt Chrome
         EdgeDriver driver = new EdgeDriver();
-        driver.get("https://www.demoblaze.com/#");
-        Thread.sleep(10000);
 
-//        Category
-        WebElement butotnPhone = driver.findElement(By.xpath("//a[@onclick=\"byCat('phone')\"]"));
-        WebElement butotnNotebook = driver.findElement(By.xpath("//a[@onclick=\"byCat('notebook')\"]"));
-        WebElement butotnMonitor = driver.findElement(By.xpath("//a[@onclick=\"byCat('monitor')\"]"));
+        // Mở trang web
+        driver.get("https://www.demoblaze.com/");
 
-//        carousel control
-        WebElement carouselControlPrev = driver.findElement(By.xpath("//a[@class=\"carousel-control-prev\" ]"));
-        WebElement carouselControlNext = driver.findElement(By.xpath("//a[@class=\"carousel-control-prev\" ]"));
+        // Phóng to trình duyệt
+        driver.manage().window().maximize();
 
-//        menu Header
-        WebElement headerHome = driver.findElement(By.xpath("//ul[@class=\"navbar-nav ml-auto\"]/li[@class=\"nav-item active\"]"));
-        WebElement headerContact = driver.findElement(By.xpath("//ul[@class=\"navbar-nav ml-auto\"]/li[@class=\"nav-item\"]/a[@data-target=\"#videoModal\"]"));
-        WebElement headerAboutUs = driver.findElement(By.xpath("//ul[@class=\"navbar-nav ml-auto\"]/li[@class=\"nav-item\"]/a[@data-target=\"#videoModal\"]"));
-        WebElement headerCart = driver.findElement(By.xpath("//ul[@class=\"navbar-nav ml-auto\"]/li[@class=\"nav-item\"]/a[@id=\"cartur\"]"));
-        WebElement headerLogin = driver.findElement(By.xpath("//ul[@class=\"navbar-nav ml-auto\"]/li[@class=\"nav-item\"]/a[@data-target=\"#logInModal\"]"));
-        WebElement headerSignUp = driver.findElement(By.xpath("//ul[@class=\"navbar-nav ml-auto\"]/li[@class=\"nav-item\"]/a[@data-target=\"#signInModal\"]"));
+        // Khởi tạo WebDriverWait với thời gian chờ là 20 giây
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
 
-//        item product
-        WebElement imageItem = driver.findElement((By.xpath("//div[@class=\"card h-100\"]/a[@href=\"prod.html?idp_=1\"]")));
-        WebElement titleItem = driver.findElement((By.xpath("//div[@class=\"card h-100\"]/div/h4/a[@href=\"prod.html?idp_=1\"]")));
-        WebElement priceItem = driver.findElement((By.xpath("//img[@src=\"imgs/galaxy_s6.jpg\"]/parent::a[@href='prod.html?idp_=1']/following-sibling::div/h5")));
-        WebElement descItem = driver.findElement((By.xpath("//img[@src=\"imgs/galaxy_s6.jpg\"]/parent::a[@href='prod.html?idp_=1']/following-sibling::div/p")));
-//        System.out.println(descItem.getText());
-//        imageItem.click();
+        String a = "//li[@class='nav-item'][index]";
+        WebElement header;
+
+        for (int i = 1; i <= 7; i++) {
+            if (i != 5 && i != 6) {
+                header = driver.findElement((By.xpath(a.replace("index",String.valueOf(i)))));
+                wait.until(ExpectedConditions.visibilityOf(header));
+                wait.until(ExpectedConditions.elementToBeClickable(header));
+                header.click();
+
+                if(i == 1) {
+                    Thread.sleep(1000);
+                    WebElement close1 = driver.findElement(By.xpath("(//button[@type='button'][normalize-space()='Close'])[1]"));
+                    wait.until(ExpectedConditions.visibilityOf(close1));
+                    wait.until(ExpectedConditions.elementToBeClickable(close1));
+                    close1.click();
+                }
+
+                if(i == 2) {
+                    Thread.sleep(1000);
+                    WebElement close2 = driver.findElement(By.xpath("(//button[@type='button'][normalize-space()='Close'])[4]"));
+                    wait.until(ExpectedConditions.visibilityOf(close2));
+                    wait.until(ExpectedConditions.elementToBeClickable(close2));
+                    close2.click();
+                }
+
+                if(i == 3) {
+                    Thread.sleep(1000);
+                    driver.navigate().back();
+                }
+
+                if(i == 4) {
+                    Thread.sleep(1000);
+                    WebElement close4 = driver.findElement(By.xpath("(//button[@type='button'][normalize-space()='Close'])[3]"));
+                    wait.until(ExpectedConditions.visibilityOf(close4));
+                    wait.until(ExpectedConditions.elementToBeClickable(close4));
+                    close4.click();
+                }
+
+                if(i == 7) {
+                    Thread.sleep(1000);
+                    WebElement close5 = driver.findElement(By.xpath("(//button[@type='button'][normalize-space()='Close'])[2]"));
+                    wait.until(ExpectedConditions.visibilityOf(close5));
+                    wait.until(ExpectedConditions.elementToBeClickable(close5));
+                    close5.click();
+                }
+            }
+        }
+
+        String category = "(//a[normalize-space()='value'])[1]";
+        String[] listCategory = { "Phones", "Laptops", "Monitors" };
+        WebElement categorys;
+
+        for (int i = 0; i <= 3; i++) {
+            categorys = driver.findElement((By.xpath(category.replace("value",listCategory[i]))));
+            wait.until(ExpectedConditions.visibilityOf(categorys));
+            wait.until(ExpectedConditions.elementToBeClickable(categorys));
+            categorys.click();
+        }
+
+        By imageItemLocator = By.xpath("//div[@class='card h-100']/a[@href='prod.html?idp_=10']");
+        By titleItemLocator = By.xpath("//div[@class='card h-100']/div/h4/a[@href='prod.html?idp_=10']");
+        By priceItemLocator = By.xpath("//img[@src='imgs/galaxy_s6.jpg']/parent::a[@href='prod.html?idp_=10']/following-sibling::div/h5");
+        By descItemLocator = By.xpath("//img[@src='imgs/galaxy_s6.jpg']/parent::a[@href='prod.html?idp_=10']/following-sibling::div/p");
+
+
+        wait.until(ExpectedConditions.elementToBeClickable(imageItemLocator));
+        WebElement imageItem = driver.findElement(imageItemLocator);
+        imageItem.click();
+        Thread.sleep(1000);
+        driver.navigate().back();
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(titleItemLocator));
+        WebElement titleItem = driver.findElement(titleItemLocator);
+        System.out.println((titleItem.getText()));
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(priceItemLocator));
+        WebElement priceItem = driver.findElement(priceItemLocator);
+        System.out.println((priceItem.getText()));
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(descItemLocator));
+        WebElement descItem = driver.findElement(descItemLocator);
+        System.out.println((descItem.getText()));
     }
 }
